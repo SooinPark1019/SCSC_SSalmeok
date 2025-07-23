9aaeff790decf671cbe82b0ed471ad3ed18e0e79
ychangseok <147029297+ychangseok@users.noreply.github.com>
2025-07-23 17:03:53 +0900

    Update LineIntersection.cpp

--- Geometry/LineIntersection.cpp
+++ Geometry/LineIntersection.cpp
@@ -5,12 +5,12 @@ ll ccw(const PT &p1, const PT &p2, const PT &p3){
 bool isIntersect(PT A, PT B, PT C, PT D){
   // check segment AB and segment CD intersects
 
-	ll ab = ccw(A, B, C) * ccw(A, B, D);
-	ll cd = ccw(C, D, A) * ccw(C, D, B);
+    ll ab = ccw(A, B, C) * ccw(A, B, D);
+    ll cd = ccw(C, D, A) * ccw(C, D, B);
     if (ab == 0 && cd == 0){
-		if (A > B) swap(A, B);
-		if (C > D) swap(C, D);
-		return C <= B && A <= D;
-	}
-	return ab <= 0 && cd <= 0;
+	if (A > B) swap(A, B);
+	if (C > D) swap(C, D);
+	return C <= B && A <= D;
+    }
+    return ab <= 0 && cd <= 0;
 }
