ff602ea6d81332b0c6792ba1d831716b654c99e4
ychangseok <147029297+ychangseok@users.noreply.github.com>
2025-07-23 16:46:49 +0900

    Update ConvexHull.cpp

---
+++ Geometry/ConvexHull.cpp
@@ -0,0 +1,37 @@
+polygon convex_hull(polygon v){
+    // monotone chain
+    // change ccw(~~~) < 0 to ccw(~~~) <= 0 to remove colinear pts
+
+    int n = v.size();
+    int k = 0;
+
+    if (n < 3) return v;
+
+    polygon ans(2*n);
+
+    sort(all(v),
+        [](const PT &p1, const PT &p2) -> bool{
+            if (p1.x == p2.x) return p1.y < p2.y;
+            return p1.x < p2.x;
+        }
+    );
+    
+    for (int i = 0; i < n; i++){
+        while (k >= 2 && ccw(ans[k-2], ans[k-1], v[i]) < 0){
+            k--;
+        }
+        ans[k] = v[i];
+        k++;
+    }
+
+    for (int i = n- 1, t = k + 1; i > 0; i--){
+        while (k >= t && ccw(ans[k-2], ans[k-1], v[i-1]) < 0){
+            k--;
+        }
+        ans[k] = v[i-1];
+        k++;
+    }
+
+    ans.resize(k-1);
+    return ans;
+}
