27528276f94d1687581b70106df5e906c675b636
ychangseok <147029297+ychangseok@users.noreply.github.com>
2025-07-23 18:36:05 +0900

    Update AngleSort.cpp

--- Geometry/AngleSort.cpp
+++ Geometry/AngleSort.cpp
@@ -3,7 +3,7 @@ sort(all(pts),
     [&p](const PT &p1, const PT &p2) -> bool{
         if (p2 == p) return false;
         if (p1 == p) return true;
-        if (p1 > p != p > p2) return p1 < p2;
+        if (p1 > p != p2 > p) return p1 < p2;
         return ccw(p, p1, p2) > 0;
     }
 );
