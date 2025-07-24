a4020a989662d8232c64ac8e6ac1d8fc57d1c0fe
ychangseok <147029297+ychangseok@users.noreply.github.com>
2025-07-24 09:41:06 +0900

    Update CombMod.py

--- NumberTheory/CombMod.py
+++ NumberTheory/CombMod.py
@@ -1,11 +1,8 @@
-# Check correctness
-
 def fact_mod_prime(n, p):
     # this method is useful when p is small
+    # O(log n)
 
-    # calculating n! % p
-    # n! = p^M r
-    # r < p
+    # n! = p^M r (r < p)
     # return [M, r]
 
     # fact[i] = i! % p
@@ -17,22 +14,19 @@ def fact_mod_prime(n, p):
     k, r = n // p, n % p
     K = fact_mod_prime(k, p)
 
-    mod = p
-    k_, r_ = n // mod, n % mod
+    k_, r_ = n // p, n % p
     m_ = fact[r_]
     if k_ % 2 == 1:
-        m_ = mod - m_
+        m_ = p - m_
 
     m = m_ * K[1]
-    m %= mod
+    m %= p
 
     k += K[0]
     return [k, m]
 def comb_mod_prime(n, k, p):
-    # calculating nCk % p
-    # returns [k, m] such that
-    # nCk = p^k m
-    # 0 <= m < p
+    # nCk = p^M r (m < p)
+    # return [M, r]
 
     a = fact_mod_prime(n, p)
     b = fact_mod_prime(k, p)
@@ -40,7 +34,7 @@ def comb_mod_prime(n, k, p):
     mod = p
 
     t = a[0] - b[0] - c[0]
-    n = a[1] * pow(b[1], 17, mod) * pow(c[1], 17, mod)
+    n = a[1] * pow(b[1], -1, mod) * pow(c[1], -1, mod)
 
     return [t, n]
 
@@ -82,7 +76,7 @@ def comb_mod_primetower(n, k, p, e):
     mod = p**e
 
     t = a[0] - b[0] - c[0]
-    n = a[1] * pow(b[1], 17, mod) * pow(c[1], 17, mod)
+    n = a[1] * pow(b[1], -1, mod) * pow(c[1], -1, mod)
 
     ans = pow(p, t, mod)*n
     return ans % mod
