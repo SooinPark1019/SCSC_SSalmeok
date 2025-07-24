53240399055792d8666100f6b1873a6b1d2f46e9
ychangseok <147029297+ychangseok@users.noreply.github.com>
2025-07-24 09:23:32 +0900

    Update Farey.py

--- NumberTheory/Farey.py
+++ NumberTheory/Farey.py
@@ -1,10 +1,6 @@
 def nextTerm(n, a, b):
-    # TODO : check correctness
-  
-    # next term of (a/b) in farey sequence
-    # error if doesn't exists
+    # next term of (a/b) in farey sequence F_n
     d = pow(a, phi(b) - 1, b)
-
     d *= b - 1
     d %= b
 
@@ -14,17 +10,15 @@ def nextTerm(n, a, b):
 def farey(n, K):
     # return K-th term of farey sequence
     # -1 if doesn't exist
-    a, b = map(int, input().split())
-    c, d = nextTerm(n, a, b)
+    a, b, c, d = 0, 1, 1, n
 
     cnt = 0
-
+    if K == 1: return (a, b)
     while c <= n:
         k = (n + b) // d
         a, b, c, d = c, d, k*c-a, k*d-b
 
         cnt += 1
         if cnt == K:
-            print(f"{a} {b}")
-            exit(0)
-    print(-1)
+            return (a, b)
+    return -1
